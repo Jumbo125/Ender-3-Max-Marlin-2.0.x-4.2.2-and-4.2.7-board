18dd2762dde0a1ae23d80c19465486f913517276
Jumbo125 <61667987+Jumbo125@users.noreply.github.com>
2021-12-06 10:42:46 +0100

    upload source code
    upload source code

---
+++ source code/Marlin/_Statusscreen.h
@@ -0,0 +1,63 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * Custom Status Screen bitmap
+ *
+ * Place this file in the root with your configuration files
+ * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
+ *
+ * Use the Marlin Bitmap Converter to make your own:
+ * https://marlinfw.org/tools/u8glib/converter.html
+ */
+
+//
+// Status Screen Logo bitmap
+//
+#define STATUS_LOGO_Y            8
+#define STATUS_LOGO_WIDTH       39
+
+const unsigned char status_logo_bmp[] PROGMEM = {
+  B00000000,B00000000,B00000000,B00000110,B00110000,
+  B00001000,B10001001,B11100100,B00001001,B01001000,
+  B00011100,B11001001,B00110100,B00000110,B00110000,
+  B00010100,B11101001,B00010100,B00000000,B10000000,
+  B00010100,B10101001,B00010100,B00000000,B10000000,
+  B00111110,B10011001,B00010100,B00000000,B10000000,
+  B00100010,B10011001,B00110100,B00001000,B00010000,
+  B00100010,B10001001,B11100100,B00000111,B11100000
+};
+
+//
+// Use default bitmaps
+//
+#define STATUS_HOTEND_ANIM
+#define STATUS_BED_ANIM
+#define STATUS_HEATERS_XSPACE   20
+#if HOTENDS < 2
+  #define STATUS_HEATERS_X      48
+  #define STATUS_BED_X          72
+#else
+  #define STATUS_HEATERS_X      40
+  #define STATUS_BED_X          80
+#endif
